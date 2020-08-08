bf6c75bdf7ec3e91ae83803306ac17b798fa9490
Dominique Brisco <dbrisco93@gmail.com>
2020-08-08 06:45:31 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -2,6 +2,6 @@ def using_push(colors_in_the_rainbow, string)
   colors_in_the_rainbow.push("violet")
 end
 
-def using_unshift(bouroughs_in_nyc, "Staten Island")
+def using_unshift(bouroughs_in_nyc, string)
   bouroughs_in_nyc.unshift("Staten Island")
 end
