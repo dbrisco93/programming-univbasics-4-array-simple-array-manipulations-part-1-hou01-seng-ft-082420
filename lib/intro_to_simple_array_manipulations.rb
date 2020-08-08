b2b228c9545ef92a7adaab0bc96a0a06c9bbc412
Dominique Brisco <dbrisco93@gmail.com>
2020-08-08 06:23:08 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,5 +1,5 @@
 def using_push
-  colors_in_the_rainbow = ["red", "orange", "yellow", "green", "blue", "indigo"]
+  using_push = ["red", "orange", "yellow", "green", "blue", "indigo"]
   next_color = "violet"
-  colors_in_the_rainbow.push(next_color)
+  using_push.push(next_color)
 end
